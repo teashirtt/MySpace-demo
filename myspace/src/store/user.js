54ee92d1ad72fadb740e3340952fee448f34b624
teashirtt <1223696764@qq.com>
2022-06-22 19:57:01 +0800

    add vuex

---
+++ myspace/src/store/user.js
@@ -0,0 +1,18 @@
+const ModuleUser = {
+    state: {
+        id: "",
+        username: "",
+        photo: "",
+        followerCount: 0,
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+};
+
+export default ModuleUser;
